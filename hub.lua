e5e559b2bd0b4242828915c72019c6c1123990ee
BR3XALITY <marioplays254@gmail.com>
2025-02-17 10:47:54 -0800

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,9 +1,8 @@
 game:GetService("StarterGui"):SetCore("SendNotification", {
-Title = "Made By BR3XALITY";
-Text = "@boostedreality On YT";
-Icon = "rbxassetid://110743274616354"})
-Duration = 16;
-
+    Title = "Made By BR3XALITY";
+    Text = "@boostedreality On YT";
+    Icon = "rbxassetid://110743274616354"
+})
 wait(3.0)
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/DenDenZZZ/Kavo-UI-Library/main/Kavo.lua"))()
@@ -18,5 +17,5 @@ Section1:NewButton("Fly GUI V3", "Allows You To Fly", function()
 end)
 
 Section1:NewButton("Noclip", "Toggleable Noclipping", function()
-    loadstring(game:HttpGet('https://gist.githubusercontent.com/BR3XALITY/2f343c8bdfb2a6d254ba45cb191ab9e2/raw/073c9c09a874958264386abe50f5554a66fd849e/fly.lua'))()
+    loadstring(game:HttpGet('https://gist.githubusercontent.com/BR3XALITY/2f343c8bdfb2a6d254ba45cb191ab9e2/raw/073c9c09a874958264386abe50f5554a66fd849e/noclip.lua'))()
 end)
