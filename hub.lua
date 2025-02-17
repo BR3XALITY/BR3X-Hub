ed1c76d732dcbb5f28c0fec8c279357ab56a7188
BR3XALITY <marioplays254@gmail.com>
2025-02-17 10:28:06 -0800

    Create hub.lua

---
+++ hub.lua
@@ -0,0 +1,11 @@
+game:GetService("StarterGui"):SetCore("SendNotification", {
+Title = "Made By BR3XALITY";
+Text = "@boostedreality On YT";
+Icon = "rbxassetid://110743274616354"})
+Duration = 16;
+
+wait(3.0)
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/DenDenZZZ/Kavo-UI-Library/main/Kavo.lua"))()
+
+local Window = Library.CreateLib("BR3X Hub", "DarkTheme")
